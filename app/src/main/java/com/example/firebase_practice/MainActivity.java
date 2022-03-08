2eca62934b72907eeb6f2049fe481aa5c4e28e6a
Syed Bilal Abbas <abbasb000@gmail.com>
2022-03-08 14:34:33 +0500

    Data delete

--- app/src/main/java/com/example/firebase_practice/MainActivity.java
+++ app/src/main/java/com/example/firebase_practice/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -17,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
     public TextView textView;
+    public Button btnDelete;
+    public Button btnLoad;
+    public Task<DataSnapshot> tasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView) findViewById(R.id.textView1);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnLoad = findViewById(R.id.btnLoad);
 
 //        FirebaseDatabase database = FirebaseDatabase.getInstance();
 //        DatabaseReference myRef = database.getReference("message");
@@ -34,27 +42,55 @@ public class MainActivity extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
         mDatabase.child("message").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
+                tasks = task;
+
 
                 if (!task.isSuccessful()) {
                     Log.e("firebase12345", "Error getting data", task.getException());
+                    Toast.makeText(getApplicationContext(), "Not", Toast.LENGTH_SHORT).show();
                 }
                 else {
                     Log.d("firebase12345", String.valueOf(task.getResult().getValue()));
-                    textView.setText(String.valueOf(task.getResult().getValue()));
+                    Toast.makeText(getApplicationContext(), "Yes", Toast.LENGTH_SHORT).show();
+
 
                 }
             }
+
+
+
         });
 
 //        textView.setText("H");
 
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mDatabase.child("message").removeValue();
+                Toast.makeText(getApplicationContext(), "Deleted succesfully", Toast.LENGTH_SHORT).show();
+                tasks = null;
+                textView.setText("");
+            }
+        });
+
+        btnLoad.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (tasks != null)
+                    textView.setText(String.valueOf(tasks.getResult().getValue()));
+
+            }
+        });
 
 
 
 
 
 
-        }
+
+
+
+
+    }
 }
