b6a2a4cef2d04fec3327e01786d7ca0f726c16a2
Syed Bilal Abbas <abbasb000@gmail.com>
2022-03-08 12:37:35 +0500

    Data read

--- app/src/main/java/com/example/firebase_practice/MainActivity.java
+++ app/src/main/java/com/example/firebase_practice/MainActivity.java
@@ -1,22 +1,60 @@
 package com.example.firebase_practice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    public TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        textView = (TextView) findViewById(R.id.textView1);
 
-        myRef.setValue("Hello, World!");
-    }
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("message");
+
+//        myRef.setValue("Hello, World!");
+
+
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("message").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+
+                if (!task.isSuccessful()) {
+                    Log.e("firebase12345", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("firebase12345", String.valueOf(task.getResult().getValue()));
+                    textView.setText(String.valueOf(task.getResult().getValue()));
+
+                }
+            }
+        });
+
+//        textView.setText("H");
+
+
+
+
+
+
+
+        }
 }
